bceef29bb9e237ad4156a136ef869766577c758b
Felipe Carvalho <karvalho_182@hotmail.com>
2022-12-04 23:07:13 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -24,9 +24,17 @@ describe('Bcrypt Adapter', () => {
   })
 
   test('Shuld return a hash on success', async () => {
-    const salt = 12
-    const sut = makeSut(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if Bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
